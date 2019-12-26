c4ed66f8c12f5552f69af6784a57e45e5b5d572c
Mohammad Amin <mak12776@gmail.com>
2019-12-26 03:58:18 +0330

    more

--- java-libs/src/libs/machines/BufferMachine.java
+++ java-libs/src/libs/machines/BufferMachine.java
@@ -1,23 +1,55 @@
 package libs.machines;
 
-public class BufferMachine 
-{	
-	private byte[] instBuffer;
+public class BufferMachine
+{
 	private byte[][] buffers;
-	
-	private int[] ip;
 	private int[] dp;
 	
-	public BufferMachine(byte[] instBuffer, int[] startInst, int numberOfBuffers, int numberOfPointers)
+	private int bip;
+	private int ip;
+	
+	public BufferMachine(byte[][] buffers, int[] pointers, int baseInstPointer, int instPointer)
 	{
-		this.instBuffer = instBuffer;
-		this.ip = new int[] {startInst[0], startInst[1]};
-		this.buffers = new byte[numberOfBuffers][];
-		this.dp = new int[numberOfPointers];
+		this.buffers = buffers;
+		this.dp = pointers;
+		
+		this.bip = baseInstPointer;
+		this.ip = instPointer;
 	}
+	
+	private byte nextByte()
+	{
+		return this.buffers[bip][ip++];
+	}
+	
+	private short nextShort()
+	{
+		return 	(short) (
+				((this.buffers[bip][ip++] & 0xff) << 8) | 
+				(this.buffers[bip][ip++] & 0xff)
+				);
+	}
+	
+	private int nextInt()
+	{
+		return 	((this.buffers[bip][ip++] & 0xff) << 24) | 
+				((this.buffers[bip][ip++] & 0xff) << 16) |
+				((this.buffers[bip][ip++] & 0xff) << 8) | 
+				(this.buffers[bip][ip++] & 0xff);
+	}
+	
+	// instructions
+	
+	public final byte INST_NOOP = 0x00;
 	
 	public void run()
 	{
+		byte inst;
 		
+		while (true)
+		{
+			inst = nextByte();
+			
+		}
 	}
 }
--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -5,5 +5,9 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+		
+		byte a = (byte) 0xff;
+		
+		System.out.println(Integer.toHexString((int)(a & 0xff)));
 	}
 }
