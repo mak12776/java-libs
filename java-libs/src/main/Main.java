105a03cea03d0442e26e4e969f4a6f903da1a0c4
Mohammad Amin <mak12776@gmail.com>
2019-12-27 23:31:51 +0330

    more

--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -4,10 +4,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
 		
-		byte a = (byte) 0xff;
-		
-		System.out.println(Integer.toHexString((int)(a & 0xff)));
 	}
 }
