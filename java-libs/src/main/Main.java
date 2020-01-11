e12bea8f423b5469e650d673083269859d7fb03a
Mohammad Amin <mak12776@gmail.com>
2020-01-12 02:58:34 +0330

    backup

--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -1,19 +1,20 @@
 package main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import jeneric.ast.mains.JavaFunction;
 import jeneric.modifiers.JavaAccessModifier;
 
 public class Main 
 {
+	class A extends java.util.LinkedList<Integer>
+	{
+		
+	}
+	
 	public static void main(String[] args) throws IOException
 	{
-		JavaFunction function = new JavaFunction();
 		
-		function.accessModifier = JavaAccessModifier.PUBLIC;
-		function.isStatic = false;
-		function.isFinal = false;
-		function.name = "appendByte";
 	}
 }
