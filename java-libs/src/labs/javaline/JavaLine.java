00ba8fce7647777b345c38554890e8ad967bb82e
Mohammad Amin <mak12776@gmail.com>
2020-01-28 15:23:43 +0330

    backup

--- java-libs/src/labs/javaline/JavaLine.java
+++ java-libs/src/labs/javaline/JavaLine.java
@@ -1,12 +1,20 @@
 package labs.javaline;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class JavaLine
 {
-	public static void generate(InputStream inputStream, OutputStream outputStream, GeneratorSettings settings)
+	public static void generate(InputStream inputStream, OutputStream outputStream, GeneratorSettings settings) throws IOException
 	{
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		String line;
+		while ((line = reader.readLine()) != null)
+		{
+			
+		}
 	}
 }
--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -5,13 +5,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println(Integer.MAX_VALUE);
-		System.out.println(Integer.MIN_VALUE);
-		System.out.println("----");
 		
-		int a = Integer.MIN_VALUE;
-		int b = -a;
-		
-		System.out.println(b);
 	}
 }
