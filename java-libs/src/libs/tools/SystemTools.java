52bcf485690bc04307626dbca12d62153c3cc074
Mohammad Amin <mak12776@gmail.com>
2020-01-29 20:17:09 +0330

    backup

--- java-libs/src/libs/tools/SystemTools.java
+++ java-libs/src/libs/tools/SystemTools.java
@@ -4,6 +4,13 @@ import java.io.File;
 
 public class SystemTools
 {
+	public static final String JDK_180_202 = "C:\\Program Files\\Java\\jdk1.8.0_202"; 
+	
+	public static void setJavaHome(String path)
+	{
+		System.setProperty("java.home", path);
+	}
+	
 	public static String getProgramName()
 	{
 		return new File(SystemTools.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -1,15 +1,10 @@
 
 package main;
 
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
-
 public class Main
 {
 	public static void main(String[] args)
 	{
-		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		
 		
 	}
 }
