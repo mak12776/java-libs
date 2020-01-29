4fe6d51cbcaa405598e97924eaf3fedf5ef4b13f
Mohammad Amin <mak12776@gmail.com>
2020-01-29 19:59:58 +0330

    backup

--- java-libs/src/machines/buffer/BufferMachine.java
+++ java-libs/src/machines/buffer/BufferMachine.java
@@ -2,6 +2,8 @@ package machines.buffer;
 
 public class BufferMachine
 {
+	public static final short INST_NOOP = 0;
+	
 	
 	
 	public static void run(byte[] staticBuffer, byte[][] dynamicbuffers, int[] pointers, byte flag, int bip, int ip)
--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -1,10 +1,15 @@
 
 package main;
 
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		
 		
 	}
 }
