3660a67c104e9fa02ba6c20fd44561a202d2362c
Mohammad Amin <mak12776@gmail.com>
2020-02-04 22:53:36 +0330

    backup

--- java-libs/src/libs/tools/types/ArrayTools.java
+++ java-libs/src/libs/tools/types/ArrayTools.java
@@ -13,48 +13,4 @@ public class ArrayTools
 
 		return total;
 	}
-
-	public static byte[] newArray(int size, byte value)
-	{
-		byte[] result;
-
-		result = new byte[size];
-		for (int i = 0; i < size; i += 1)
-			result[i] = value;
-
-		return result;
-	}
-
-	public static short[] newArray(int size, short value)
-	{
-		short[] result;
-
-		result = new short[size];
-		for (int i = 0; i < size; i += 1)
-			result[i] = value;
-
-		return result;
-	}
-
-	public static int[] newArray(int size, int value)
-	{
-		int[] result;
-
-		result = new int[size];
-		for (int i = 0; i < size; i += 1)
-			result[i] = value;
-
-		return result;
-	}
-
-	public static double[] newArray(int size, double value)
-	{
-		double[] result;
-
-		result = new double[size];
-		for (int i = 0; i < size; i += 1)
-			result[i] = value;
-
-		return result;
-	}
 }
