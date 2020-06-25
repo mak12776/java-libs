ab16251bda803738d444a350358d17f079db8e7d
Mohammad Amin <mak12776@gmail.com>
2020-06-26 01:32:23 +0430

    more

--- java-libs/src/main/Main.java
+++ java-libs/src/main/Main.java
@@ -3,10 +3,20 @@ package main;
 
 import java.io.IOException;
 
+import libs.tools.SystemTools;
+
 public class Main
 {
 	public static void main(String[] args) throws IOException
 	{
-
+		
+		
+		if (args.length != 1)
+		{
+			System.out.println("usage: " + SystemTools.getProgramName() + "File to read");
+			return;
+		}
+		
+		
 	}
 }
