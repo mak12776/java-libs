592c34b18023f0be4e10b379db4ca4ffebe41b15
Mohammad Amin <mak12776@gmail.com>
2020-11-26 01:34:44 +0330

    more

---
+++ java-libs/src/main/FileCounter.java
@@ -0,0 +1,24 @@
+package main;
+
+public class FileCounter
+{
+	public static class Info
+	{
+		public long lineNumber;
+		public long words;
+		public long digits;
+		
+		public void count(byte[] buffer)
+		{
+			int index = 0;
+			
+			while (index < buffer.length)
+			{
+				if (buffer[index] == ' ')
+				{
+					
+				}
+			}
+		}
+	}
+}
